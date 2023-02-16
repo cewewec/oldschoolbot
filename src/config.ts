b37d78689ab220c68f3b33b10e030bb059cf9176
cewewec <87590596+cewewec@users.noreply.github.com>
2023-02-16 15:03:39 -0600

    Update and rename config.example.ts to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { IDiscordSettings } from './lib/types';
 
-export const botToken = '';
+export const botToken = 'MTA3NTg4Mjg5MzIwMjcwNjUxMg.G7vwVw.hMktQ-LMX3C22SqcTODwDyMNXYb48KDkAf5tmQ';
 export const production = false;
 export const patreonConfig: null | {
 	campaignID: number;
@@ -10,16 +10,16 @@ export const patreonConfig: null | {
 export const SENTRY_DSN: string | null = null;
 export const HTTP_PORT = 1234;
 export const CLIENT_SECRET = '';
-export const CLIENT_ID = '';
-export const DEV_SERVER_ID = '';
+export const CLIENT_ID = '1075882893202706512';
+export const DEV_SERVER_ID = '1074062310697607258';
 export const GITHUB_TOKEN = '';
 export const DISCORD_SETTINGS: Partial<IDiscordSettings> = {
 	// Your bot unique ID goes here
 	BotID: '729244028989603850'
 };
 // Add or replace these with your Discord ID:
-export const OWNER_IDS = ['157797566833098752'];
-export const ADMIN_IDS = ['425134194436341760'];
+export const OWNER_IDS = ['1062848592517275649'];
+export const ADMIN_IDS = ['1062848592517275649'];
 export const MAXING_MESSAGE = 'Congratulations on maxing!';
 // Discord server where admin commands will be allowed:
-export const SupportServer = '940758552425955348';
+export const SupportServer = '1074062310697607258';
